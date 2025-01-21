be5f3692fad51a2247ed81677e8702184684b260
harriet221 <potter19@dongguk.edu>
2025-01-21 16:40:07 +0900

    Feat: 디데이 수정 구현 + 조회 얼개만 구현

--- src/main/java/com/pape/timetodo/domain/main/service/DdayService.java
+++ src/main/java/com/pape/timetodo/domain/main/service/DdayService.java
@@ -1,9 +1,15 @@
 package com.pape.timetodo.domain.main.service;
 
+import com.pape.timetodo.domain.main.model.dday.RegisterDdayRQ;
 import com.pape.timetodo.domain.main.model.dday.RegisterDdayRS;
-import com.pape.timetodo.domain.main.model.dday.registerDdayRQ;
+import com.pape.timetodo.domain.main.model.dday.UpdateDdayRQ;
+import com.pape.timetodo.domain.main.model.dday.UpdateDdayRS;
+import com.pape.timetodo.global.constant.StatusType;
+import com.pape.timetodo.global.exception.AppException;
+import com.pape.timetodo.global.exception.ExceptionCode;
 import com.pape.timetodo.global.jpa.entity.DdayEntity;
 import com.pape.timetodo.global.jpa.entity.UsersEntity;
+import com.pape.timetodo.global.jpa.repository.DdayQueryRepository;
 import com.pape.timetodo.global.jpa.repository.DdayRepository;
 import com.pape.timetodo.global.util.UserUtil;
 import lombok.RequiredArgsConstructor;
@@ -11,14 +17,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class DdayService {
 
+    private final UserUtil userUtil;
+
     private final DdayRepository ddayRepository;
 
-    private final UserUtil userUtil;
+    private final DdayQueryRepository ddayQueryRepository;
 
     /**
      * 디데이 등록
@@ -26,7 +37,7 @@ public class DdayService {
      * @return RegisterDdayRS
      */
     @Transactional
-    public RegisterDdayRS registerDday(registerDdayRQ rq) {
+    public RegisterDdayRS registerDday(RegisterDdayRQ rq) {
 
         UsersEntity usersEntity = userUtil.getUsersEntity();
 
@@ -45,4 +56,48 @@ public class DdayService {
 
         return result;
     }
+
+
+    /**
+     * 디데이 수정
+     * @param rq UpdateDdayRQ
+     * @return UpdateDdayRS
+     */
+    @Transactional
+    public UpdateDdayRS updateDday(UpdateDdayRQ rq) {
+
+        DdayEntity ddayEntity = ddayQueryRepository.findByIdAndUsersEntity(rq.getIdx(), userUtil.getUsersEntity());
+
+        if(ddayEntity == null) throw new AppException(ExceptionCode.DATA_NOT_FIND);
+
+        if(rq.getContent() != null) ddayEntity.setContent(rq.getContent());
+        if(rq.getTargetDt() != null) ddayEntity.setTargetDt(rq.getTargetDt());
+
+        ddayEntity.setUpdateDt(LocalDateTime.now());
+        ddayRepository.save(ddayEntity);
+
+        UpdateDdayRS result = new UpdateDdayRS();
+        result.setUpdateDt(ddayEntity.getUpdateDt());
+
+        return result;
+    }
+
+
+    /**
+     * 디데이 삭제 [논리 삭제]
+     * @param idx Long
+     */
+    public void deleteDday(Long idx) {
+        UsersEntity usersEntity = userUtil.getUsersEntity();
+
+        Optional<DdayEntity> ddayEntityWrapper = ddayRepository.findByIdxAndUsersEntity(idx, usersEntity);
+
+        if(ddayEntityWrapper.isPresent()){
+
+            DdayEntity ddayEntity = ddayEntityWrapper.get();
+            ddayEntity.setDeleteDt(LocalDateTime.now());
+            ddayEntity.setStatus(StatusType.DELETED.getValue());
+        }
+    }
+    
 }
