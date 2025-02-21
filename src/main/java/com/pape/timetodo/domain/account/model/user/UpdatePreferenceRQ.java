4da5a2bfb3ce249c8fbdbcf2574bd0b32f79050a
harriet221 <potter19@dongguk.edu>
2025-02-21 15:44:50 +0900

    Refactor: Swagger 편의를 위해... example에 적절한 예시 가게 하느라 약간의 수정

--- src/main/java/com/pape/timetodo/domain/account/model/user/UpdatePreferenceRQ.java
+++ src/main/java/com/pape/timetodo/domain/account/model/user/UpdatePreferenceRQ.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.util.Set;
-
 @Data
 public class UpdatePreferenceRQ {
 
@@ -17,7 +16,7 @@ public class UpdatePreferenceRQ {
               "values": ["D_REGISTRATION_ORDER", "D_COMPLETE_ORDER", "D_SWIPE_CHECK"]
             }
             """)
-    private PreferenceUpdate<SortType> ddaySortType;
+    private DdaySortPreference ddaySortType;
 
     @Schema(description = "카테고리 TODO 정렬 설정",
             example = """
@@ -26,7 +25,7 @@ public class UpdatePreferenceRQ {
               "values": ["C_REGISTRATION_ORDER", "C_COMPLETE_ORDER"]
             }
             """)
-    private PreferenceUpdate<SortType> categorySortTypes;
+    private CategorySortPreference categorySortTypes;
 
     @Schema(description = "알림 설정",
             example = """
@@ -35,13 +34,23 @@ public class UpdatePreferenceRQ {
               "values": ["START_TODO_NOTI", "TARGET_DDAY_NOTI", "FRIEND_NEW_TODO_NOTI", "FRIEND_FOLLOW_NOTI"]
             }
             """)
-    private PreferenceUpdate<NotificationType> notificationTypes;
+    private NotificationPreference notificationTypes;
 
     @Data
-    public static class PreferenceUpdate<T> {
-
+    public static class DdaySortPreference {
         private boolean update;
-        private Set<T> values;
+        private Set<SortType> values;
+    }
 
+    @Data
+    public static class CategorySortPreference {
+        private boolean update;
+        private Set<SortType> values;
+    }
+
+    @Data
+    public static class NotificationPreference {
+        private boolean update;
+        private Set<NotificationType> values;
     }
 }
