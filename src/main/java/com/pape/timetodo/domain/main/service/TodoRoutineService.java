1974f2817f6622a2e56df646cdc722b9da804bc2
harriet221 <potter19@dongguk.edu>
2025-02-24 21:33:48 +0900

    Update: 홈화면 디데이 조회에도 설정 옵션 반영

--- src/main/java/com/pape/timetodo/domain/main/service/TodoRoutineService.java
+++ src/main/java/com/pape/timetodo/domain/main/service/TodoRoutineService.java
@@ -9,6 +9,7 @@ import com.pape.timetodo.domain.main.model.todo.*;
 import com.pape.timetodo.domain.main.model.todo.GetTodoTimerHistoryRs.TimerHistory;
 import com.pape.timetodo.domain.main.model.todo.RegistTodoTimerRQ.TimeData;
 import com.pape.timetodo.global.constant.DayWeekType;
+import com.pape.timetodo.global.constant.SortType;
 import com.pape.timetodo.global.constant.StatusType;
 import com.pape.timetodo.global.exception.AppException;
 import com.pape.timetodo.global.exception.ExceptionCode;
@@ -121,8 +122,10 @@ public class TodoRoutineService {
         UsersEntity usersEntity = userUtil.getUsersEntity();
 
         LocalDate date = rq.getDate();
+        UserPreferencesEntity preferencesEntity = usersEntity.getUserPreferences();
+        List<SortType> ddaySortTypeList = preferencesEntity.getDdaySortType().stream().toList();
 
-        List<HomeDdayModel> ddayList = todoQueryRepository.findDdayTodoByUsersEntity(usersEntity, date);
+        List<HomeDdayModel> ddayList = todoQueryRepository.findDdayTodoByUsersEntity(usersEntity, date, ddaySortTypeList);
 
         List<HomeCategoryTodoModel> categoryTodoList = this.getMyCategoryList(usersEntity);
 
